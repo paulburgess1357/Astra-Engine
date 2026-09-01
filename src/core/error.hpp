ce07a0cc6ca2c4a31c0509197cdd356a3ba2c506
Paul B <paulburgess1357@gmail.com>
2026-08-31 19:53:02 -0600

    format

--- src/core/error.hpp
+++ src/core/error.hpp
@@ -12,7 +12,8 @@ class Error : public std::runtime_error {
   using std::runtime_error::runtime_error;
 
   template <typename... Args>
-  explicit Error(std::format_string<Args...> fmt, Args&&... args) : std::runtime_error(std::format(fmt, std::forward<Args>(args)...)) {}
+  explicit Error(std::format_string<Args...> fmt, Args&&... args)
+      : std::runtime_error(std::format(fmt, std::forward<Args>(args)...)) {}
 };
 
 }  // namespace astra::core
--- src/core/log.hpp
+++ src/core/log.hpp
@@ -2,8 +2,7 @@
 
 namespace astra::core {
 
-// Configures the process-wide spdlog default logger. Call once at startup;
-// afterwards use SPDLOG_INFO / SPDLOG_WARN / ... from any thread.
+// Set logging pattern and level for spdlog
 auto initLogging() -> void;
 
 }  // namespace astra::core
